e58720aa493a85698709d64bec53c8d21bde60fe
en3sctl <catalenes6@gmail.com>
2021-11-08 20:14:47 +0100

    Learning Git

--- src/Example2.java
+++ src/Example2.java
@@ -2,28 +2,29 @@ import javax.swing.*;
 import java.util.Scanner;
 
 public class Example2 {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Please Enter The Value Of a: ");
-        double a = scan.nextDouble();
-        System.out.println("Please Enter The Value Of b: ");
-        double b = scan.nextDouble();
-        System.out.println("Please Enter The Value Of c: ");
-        double c = scan.nextDouble();
 
-        double res =  b * b - 4.0 * a * c;
+        public static void main(String[] args) {
+            Scanner scan = new Scanner(System.in);
+            System.out.println("Please Enter The Value Of a: ");
+            double a = scan.nextDouble();
+            System.out.println("Please Enter The Value Of b: ");
+            double b = scan.nextDouble();
+            System.out.println("Please Enter The Value Of c: ");
+            double c = scan.nextDouble();
 
-        if(res<0)
-            System.out.println("Nothing");
-        else
+            double res =  b * b - 4.0 * a * c;
+
+            if(res<0)
+                System.out.println("Nothing");
+            else
             if (res == 0)
                 System.out.println("x= " + (-b / (2 * a)));
-        else{
-            System.out.println("x1= " + ((-b-Math.sqrt(res))/(2*a))+ "x2= "+(-b-Math.sqrt(res))/(2*a));
+            else{
+                System.out.println("x1= " + ((-b-Math.sqrt(res))/(2*a))+ "x2= "+(-b-Math.sqrt(res))/(2*a));
 
-            JOptionPane.showMessageDialog(null,res);
+                JOptionPane.showMessageDialog(null,res);
 
+            }
         }
-    }
 }
 
