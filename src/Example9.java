5457c23632591a84076d8484aa8c15a714825496
en3sctl <catalenes6@gmail.com>
2021-11-10 00:00:55 +0100

    Learning Git

--- src/Example9.java
+++ src/Example9.java
@@ -5,8 +5,8 @@ public class Example9{
 
         Scanner scan = new Scanner(System.in);
         int N = scan.nextInt();
-
         for(int i=1; i<=10; i++){
+
             System.out.println(N + " x " + i + " = " + (N*i));
         }
     }
