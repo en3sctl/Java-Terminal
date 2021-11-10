561ca9483754453c69595b609f7b90b938b38d83
en3sctl <catalenes6@gmail.com>
2021-11-10 19:54:32 +0100

    Learning Git

--- src/Example13.java
+++ src/Example13.java
@@ -1,11 +1,27 @@
-public class Example13 {
-    public Integer factorial(Integer n) {
 
-        int factorial = n;
-        for (int j = n - 1; j > 0; j--) {
-            factorial = factorial * j;
+import java.util.Arrays;
+public class Example13 {
+    public static void main(String[] args) {
+
+        int []arr = new int[4];
+        int []arr1 = {1,4,2,5};
+        for(int i = 0; i< arr.length; i++){
+            System.out.println(arr[i]+" ");
+
+            int tmp [];
+            arr1[0] = 2;
+            arr1[1] = 5;
+            arr1[2] = 1;
+            arr1[3] = 4;
+
+            tmp = arr;
+            tmp = arr1;
+            arr = tmp;
+
+            System.out.println(Arrays.toString(arr));
+            System.out.println(Arrays.toString(arr1));
+
         }
-        return factorial;
     }
 }   
    
