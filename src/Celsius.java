b3c5736215c138eba05dead45191b3b86c71e03b
Enes Catal <93399183+en3sctl@users.noreply.github.com>
2021-11-13 12:23:15 +0100

    Add files via upload

---
+++ src/Celsius.java
@@ -0,0 +1,13 @@
+package com.company;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter A Degree In Celsius: ");
+        double celsius = scan.nextDouble();
+        double convertToFahrenheit = 9.0/5 * celsius + 32;
+
+        System.out.println("The Result Is: " + convertToFahrenheit);
+    }
+}
