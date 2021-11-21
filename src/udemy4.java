6e667d5614e3a5983281b6aa2bb495bd59523588
Enes Catal <93399183+en3sctl@users.noreply.github.com>
2021-11-21 05:44:27 +0100

    Add files via upload

---
+++ src/udemy4.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Task_45 {
+    //Udemy Challenge
+    public static void main(String[] args) {
+
+    }
+    public static boolean hasSameLastDigit(int a, int b, int c){
+        return isValid(a) && isValid(b) && isValid(c) && (a%10 == b%10 || b%10 == c%10 || c%10 == a%10);
+    }
+    public static boolean isValid(int num){
+        return (num >= 10) && (num <=1000);
+    }
+}
