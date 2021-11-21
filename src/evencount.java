a6413eba3ca18643ab11ad3d82956bbee750c966
Enes Catal <93399183+en3sctl@users.noreply.github.com>
2021-11-21 05:50:05 +0100

    Add files via upload

---
+++ src/evencount.java
@@ -0,0 +1,24 @@
+package c08;
+
+public
+    class Task0801 {
+
+    public static void main(String[] args) {
+        int[] arr = {1, 4, 2, 5, 8, 6};
+
+        int evenCount = 0;
+        for(int i=0; i < arr.length; i++)
+            if(arr[i] % 2 == 0)
+                evenCount++;
+
+        int[] brr = new int[evenCount];
+        for(int i=0; i < arr.length; i++)
+            if(arr[i] % 2 == 0)
+                brr[--evenCount] = arr[i];
+
+        System.out.print("[");
+        for(int val : brr)
+            System.out.print(" "+val);
+        System.out.println("]");
+    }
+}
