a4bdb64ac0bb797d2fa333d0dcf75400c7d64c7f
Enes Catal <93399183+en3sctl@users.noreply.github.com>
2021-11-21 05:46:19 +0100

    Update and rename udemy2.java to Task34.java

--- src/Task34.java
+++ src/Task34.java
@@ -1,6 +1,6 @@
 package com.company;
 
-public class Task_51 {
+public class Task_34 {
     //Udemy challenges
     public static void main(String[] args) {
         System.out.println(getLargestPrime(21));
