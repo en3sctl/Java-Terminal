7ecfd237f89b8d4f81fb05b1be1af9f999195596
Enes Catal <93399183+en3sctl@users.noreply.github.com>
2021-12-23 22:02:34 +0100

    Update Task59.java

--- src/Task59.java
+++ src/Task59.java
@@ -1,5 +1,7 @@
 package CarpetOBP;
-
+/*The Carpet Company has asked you to write an application that calculates the price of carpeting for rectangular rooms. 
+To calculate the price, you multiply the area of the floor (width times length) by the price per square meter of carpet. 
+For example, the area of the floor that is 12 meters long and 10 meters wide is 120 square meters. To cover the floor with a carpet that costs $8 per square meter would cost $960.*/
 public class CarpetCompanyMain {
     public static void main(String[] args) {
         Carpet carpet = new Carpet(3.5);
