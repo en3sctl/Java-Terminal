22c549842f1719963ffd37da45d77cd3353d0374
Enes Catal <93399183+en3sctl@users.noreply.github.com>
2021-12-23 21:36:55 +0100

    Update and rename ScannerHasNext.java to Task58.java

--- src/Task58.java
+++ src/Task58.java
@@ -1,6 +1,7 @@
 import java.io.*;
 import java.util.*;
-
+//HackerRank Challenge
+//Read some unknown n lines of input from stdin(System.in) until you reach EOF; each line of input contains a non-empty String.
 public class Solution {
 
     public static void main(String[] args) {
