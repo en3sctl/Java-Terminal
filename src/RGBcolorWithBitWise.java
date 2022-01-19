519bcf6671cf86b3c3aef8f08d75f7fb3b8c6d72
Enes Catal <93399183+en3sctl@users.noreply.github.com>
2022-01-19 16:46:13 +0100

    Update RGBcolorWithBitWise.java

--- src/RGBcolorWithBitWise.java
+++ src/RGBcolorWithBitWise.java
@@ -5,7 +5,8 @@ public class Task13_01 {
     public static void main(String[] args) {
 
         int a = 23, r = 121, g = 255, b = 130, color = 0;
-
+        
+        // pack four components into one int color
         color |= (a << 24);
         color |= (r << 16);
         color |= (g << 8);
@@ -19,7 +20,8 @@ public class Task13_01 {
         g = 0;
         b = 0;
 
-
+        // unpack color to get its four
+        // components back in a, r, g, b
         a = (color >> 24) & 255;
         r = (color >> 16) & 255;
         g = (color >> 8) & 255;
